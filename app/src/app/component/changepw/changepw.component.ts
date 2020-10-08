0f0352136e1e275c4e7bc5454bc9a1e7c104b775
HuuThanh81 <huynhlehuuthanh@gmail.com>
2020-10-08 21:27:42 +0700

    Em co biet gi dau :<

---
+++ app/src/app/component/changepw/changepw.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-changepw',
+  templateUrl: './changepw.component.html',
+  styleUrls: ['./changepw.component.scss'],
+})
+export class ChangepwComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ app/src/app/component/forgotpw/forgotpw.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotpw',
+  templateUrl: './forgotpw.component.html',
+  styleUrls: ['./forgotpw.component.scss'],
+})
+export class ForgotpwComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
