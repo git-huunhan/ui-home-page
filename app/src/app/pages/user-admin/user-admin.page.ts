43eb8144869ddfa8b76f04a030a08715da2383e6
HuuThanh81 <huynhlehuuthanh@gmail.com>
2020-10-11 13:29:32 +0700

    update-page-useradmin

--- app/src/app/pages/user-admin/user-admin.page.ts
+++ app/src/app/pages/user-admin/user-admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-admin',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAdminPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Cảnh báo!',
+      message: 'Bạn muốn đăng xuất ngay bây giờ ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('đã hủy tác vụ đăng xuất !');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('đăng xuất thành công');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
