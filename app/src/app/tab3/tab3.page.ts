9930996c89f581fdc1f34811f39663ac9ea3c107
HuuThanh81 <huynhlehuuthanh@gmail.com>
2020-10-11 13:27:05 +0700

    update-button

--- app/src/app/tab3/tab3.page.ts
+++ app/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Cảnh báo!',
+      message: 'Bạn muốn đăng xuất ngay bây giờ ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('đã hủy tác vụ đăng xuất !');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('đăng xuất thành công');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
